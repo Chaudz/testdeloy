7ef84e45e66edf4a18e11d94cc9c59d5a68225ac
ChauDz <buichau40@gmail.com>
2023-08-22 16:17:59 +0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello Chau");
 });
 app.listen(3000, () => {
   console.log("running");
